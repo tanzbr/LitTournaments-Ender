12743841ed539abb243554f7ab74475980560e81
Tanz <cauamelo2345@gmail.com>
2025-07-29 17:04:03 -0300

    ignore blocks placed by player

--- src/main/java/me/waterarchery/littournaments/listeners/tournamentListeners/BlockBreakListener.java
+++ src/main/java/me/waterarchery/littournaments/listeners/tournamentListeners/BlockBreakListener.java
@@ -1,9 +1,13 @@
 package me.waterarchery.littournaments.listeners.tournamentListeners;
 
+import me.waterarchery.littournaments.LitTournaments;
 import me.waterarchery.littournaments.handlers.PointHandler;
 import me.waterarchery.littournaments.handlers.TournamentHandler;
 import me.waterarchery.littournaments.models.Tournament;
 import me.waterarchery.littournaments.models.tournaments.BlockBreakTournament;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
@@ -11,10 +15,29 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class BlockBreakListener implements Listener {
+
+    private boolean enableBlockCache = false;
+    private final List<Location> placedBlocksCache = new ArrayList<>();
+
+    public BlockBreakListener() {
+        TournamentHandler tournamentHandler = TournamentHandler.getInstance();
+        List<Tournament> tournaments = tournamentHandler.getTournaments(BlockBreakTournament.class);
+
+        if (tournaments.size() > 0) {
+            enableBlockCache = true;
+        }
+
+        Bukkit.getScheduler().runTaskTimerAsynchronously(LitTournaments.getInstance(), () -> {
+            if (!enableBlockCache) return;
+            placedBlocksCache.clear();
+        }, 20*60, 20*60);
+    }
 
     @EventHandler (priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onBlockBreak(BlockBreakEvent event) {
@@ -22,12 +45,19 @@ public class BlockBreakListener implements Listener {
         TournamentHandler tournamentHandler = TournamentHandler.getInstance();
         Player player = event.getPlayer();
         Block block = event.getBlock();
+        if (placedBlocksCache.contains(block.getLocation())) return;
         World world = block.getWorld();
 
         List<Tournament> tournaments = tournamentHandler.getTournaments(BlockBreakTournament.class);
         for (Tournament tournament : tournaments) {
             pointHandler.addPoint(player.getUniqueId(), tournament, world.getName(), block.getType().name(), 1);
         }
+    }
+
+    @EventHandler (priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onBlockPlace(BlockPlaceEvent event) {
+        if (!enableBlockCache) return;
+        placedBlocksCache.add(event.getBlock().getLocation());
     }
 
 }
